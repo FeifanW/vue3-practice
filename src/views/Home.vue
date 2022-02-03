6b2380720c33c6351eed0ca3131f05d436f6e8c5
FeifanW <qihang791@163.com>
2022-02-03 16:41:14 +0800

    测试去除div标签

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld @clickBtn="test" msg="Welcome to Your Vue.js App"/>
+    <HelloWorld ref="hello" msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
@@ -18,6 +18,9 @@ export default {
     test(){
       console.log("传值成功了！")
     }
+  },
+  onMounted(){
+    this.$refs.hello.$on('clickBtn',test)
   }
 }
 </script>
