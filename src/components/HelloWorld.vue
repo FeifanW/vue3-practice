d14bab69855c4e9357c7dd7e1195d70160b35733
FeifanW <qihang791@163.com>
2022-02-17 00:30:26 +0800

    练习customRef

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="hello">
-    <div>显示数字{{obj.name}}</div>
+    <!-- <div>显示数字{{obj.name}}</div> -->
     <slot></slot>
     <button @click="btn">测试按钮</button>
-    <button @click="add">添加按钮</button>
-    <input type="text" v-model="obj.firstName">
-    <input type="text" v-model="obj.lastName">
-    <span>全名：{{obj.fullName}}</span>
-    <span v-if="obj.info.age">年龄：{{obj.info.age}}</span>
+    <!-- <button @click="add">添加按钮</button> -->
+    <!-- <input type="text" v-model="obj.firstName">
+    <input type="text" v-model="obj.lastName"> -->
+    <!-- <span>全名：{{obj.fullName}}</span> -->
+    <!-- <span>薪水：{{obj.detail.salary}}</span> -->
+    <input v-model="salary">
   </div>
 </template>
 
 <script>
-import {reactive,computed, toRef, toRaw, markRaw,
+import {reactive,computed, toRef, toRaw, markRaw, customRef,
         toRefs,shallowReactive,shallowRef, readonly, shallowReadonly} from 'vue'
 import {ref} from 'vue'
 export default {
@@ -26,36 +27,57 @@ export default {
 
     let num = ref(10)
 
-    let obj = reactive({   
-    // let obj = shallowReactive({  
-      name:'第一名',
-      firstName:'',
-      lastName:'',
-      fullName:'',
-      detail:{
-        salary:30
-      }
-    })
+    // let obj = reactive({   
+    // let obj = deal({   
+    // // let obj = shallowReactive({  
+    //   name:'第一名',
+    //   firstName:'',
+    //   lastName:'',
+    //   fullName:'',
+    //   detail:{
+    //     salary:30
+    //   }
+    // })
+    let salary = deal(10)
 
-    const info = {age:10};
-    obj.info = markRaw(info)       
-    console.log("obj", obj)
+    // const info = {age:10};
+    // obj.info = markRaw(info)       
+    // console.log("obj", obj)
+
+    function deal(value){
+      return customRef((track, trigger)=>{
+        return {
+          get(){
+            console.log("有人从自定义deal里读数据了")
+            track()
+            return value
+          },
+          set(newValue){
+            console.log("有人改数据了",newValue)
+            value = newValue   // 重新赋值
+            trigger()
+          }
+        }
+      })
+    }
 
     // 数据
     function btn(){
-      obj.info.age ++
-      console.log("age", obj.info.age)
+      salary++
+      // obj.info.age ++
+      // console.log("age", obj.info.age)
     }
-    function add(){
+    // function add(){
       // const info = {age:10};
       // obj.info = markRaw(info)       
       // console.log("obj", obj)
-    }
+    // }
     return{
       num,
-      add,
-      obj,
+      // add,
+      // obj,
       btn,
+      salary
     }
   }
 }
